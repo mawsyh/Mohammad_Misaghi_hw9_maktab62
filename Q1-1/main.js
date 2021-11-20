d9467503dfb8b702fd3ecaac56d2b0dfab9976f4
Moh <moh.mir36@gmail.com>
2021-11-20 18:23:58 +0330

    Q1-1 Done

---
+++ Q1-1/main.js
@@ -0,0 +1,16 @@
+function getName(cb) {
+  //how to take the name by user
+  //   const firstName = prompt("Please enter your first name:")
+  //   const lastName = prompt("Please enter your last name:")
+  // just testing
+  const firstName = "Ali";
+  const lastName = "Ahmadi";
+  cb(firstName, lastName);
+}
+
+//the callback function
+function typeName(firstName, lastName) {
+  console.log(`${firstName} ${lastName}`);
+}
+
+getName(typeName);
