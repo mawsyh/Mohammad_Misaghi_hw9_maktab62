0f50255a9c6b26ce327f09f76319f2e53be43814
mawsyh <moh.mir36@gmail.com>
2021-11-22 12:56:53 +0330

    Q1-5 added and done

---
+++ Q1-5/calculate.js
@@ -0,0 +1,16 @@
+let calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  devide: function (a, b) {
+    return a / b;
+  },
+};
+
+module.exports = calculator;
---
+++ Q1-5/main.js
@@ -0,0 +1,6 @@
+const calculator = require("./calculate");
+
+console.log(calculator.add(4, 5));
+console.log(calculator.subtract(4, 5));
+console.log(calculator.multiply(4, 5));
+console.log(calculator.devide(4, 5));
